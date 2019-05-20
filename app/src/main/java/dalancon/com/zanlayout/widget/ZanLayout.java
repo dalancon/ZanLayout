693d6132623a7faa91ddb994d37b69b1c5ce77d5
dalancon <jikeyuan1@sina.com>
2019-05-20 14:51:37 +0800

    自定义偏移比例实现

--- app/src/main/java/dalancon/com/zanlayout/widget/ZanLayout.java
+++ app/src/main/java/dalancon/com/zanlayout/widget/ZanLayout.java
@@ -2,12 +2,14 @@ package dalancon.com.zanlayout.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import dalancon.com.zanlayout.R;
 
 /**
  * Created by dalancon on 2019/5/20.
@@ -18,26 +20,38 @@ public class ZanLayout extends LinearLayout {
     private static final String TAG = "ZanLayout";
 
     /**
-     * 头像控件的偏移
+     * 头像控件的偏移比例
      */
-    private int itemOffset = 0;
+    private float itemOffsetPercent = 0;
 
     public ZanLayout(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public ZanLayout(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public ZanLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.zanLayout);
+
+        itemOffsetPercent = typedArray.getFloat(R.styleable.zanLayout_offset, -1);
+        if (itemOffsetPercent == -1) {
+            //如果没有设置偏移 默认偏移30%
+            itemOffsetPercent = 0.3f;
+        }
+
+        typedArray.recycle();
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int offset = getChildAt(0).getMeasuredWidth() * 1 / 3;
+        int offset = (int) (getChildAt(0).getMeasuredWidth() * itemOffsetPercent);
         setMeasuredDimension(getMeasuredWidth() - (getChildCount() - 1) * offset, getMeasuredHeight());
     }
 
@@ -78,7 +92,7 @@ public class ZanLayout extends LinearLayout {
             } else if (child.getVisibility() != GONE) {
                 final int childWidth = child.getMeasuredWidth();
                 final int childHeight = child.getMeasuredHeight();
-                itemOffset = childWidth / 3;
+                float itemOffset = childWidth * itemOffsetPercent;
 
                 final LinearLayout.LayoutParams lp =
                         (LinearLayout.LayoutParams) child.getLayoutParams();
@@ -86,8 +100,8 @@ public class ZanLayout extends LinearLayout {
                 childLeft += lp.leftMargin;
                 if (childLeft - itemOffset < 0) {//第一个头像 不需要偏移
 
-                } else { //后面的头像向左偏移 1/3的宽度
-                    childLeft = childLeft - itemOffset;
+                } else { //后面的头像向左偏移
+                    childLeft = (int) (childLeft - itemOffset);
                 }
                 child.layout(childLeft, childTop,
                         childLeft + childWidth, childHeight);
